4cf24add9a5347e895e7293b8120b4ae72096b80
Rob Avery <robertaveryiv@gmail.com>
2020-02-02 17:01:56 -0500

    Code

---
+++ Assets/ItemController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ItemController : MonoBehaviour
+{
+    public GameObject wrench;
+    public GameObject propellor;
+    public GameObject tank;
+    public GameObject antenna;
+    public GameObject wrench_empty;
+    public GameObject propellor_empty;
+    public GameObject tank_empty;
+    public GameObject antenna_empty;
+
+    public AudioSource theme;
+    public AudioSource endMusic;
+    public Canvas endGameCanvas;
+
+    private int items = 0;
+
+    public void flipItem(string type)
+    {
+        if (type.Equals("wrench"))
+        {
+            wrench.gameObject.SetActive(true);
+            wrench_empty.gameObject.SetActive(false);
+            items++;
+        }
+        else if (type.Equals("antenna"))
+        {
+            antenna.gameObject.SetActive(true);
+            antenna_empty.gameObject.SetActive(false);
+            items++;
+        }
+        else if (type.Equals("tank"))
+        {
+            tank.gameObject.SetActive(true);
+            tank_empty.gameObject.SetActive(false);
+            items++;
+        }
+        else if (type.Equals("propellor"))
+        {
+            propellor.gameObject.SetActive(true);
+            propellor_empty.gameObject.SetActive(false);
+            items++;
+        }
+
+        if (items >= 4 && type.Equals("spaceship"))
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        endGameCanvas.gameObject.SetActive(true);
+        StartCoroutine(FadeOut(theme, 7f));
+        endMusic.gameObject.SetActive(true);
+        endMusic.Play();
+        Invoke("ChangeToMainMenu", endMusic.clip.length);
+    }
+
+    public void ChangeToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public ItemController itemController;
+    public CharacterController2D characterController;
+    float horizontalMove;
+    public float runSpeed = 40f;
+    private bool jump = false;
+
+    public AudioSource collectSound;
+
+    private void Start()
+    {
+        collectSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            Destroy(other.gameObject);
+            collectSound.Play();
+            itemController.flipItem(other.gameObject.name);
+        }
+        else if (other.gameObject.CompareTag("Ship"))
+        {
+            itemController.flipItem(other.gameObject.name);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        characterController.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
